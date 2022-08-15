ff2ae341827a113cea7b47e9abdf4c4de7dd9b35
Kyle Gonzalez <kylegonzalez2255@gmail.com>
2022-08-15 22:53:13 +1000

    Clean up imports.

--- src/main/kotlin/au/kilemon/messagequeue/settings/MessageQueueSettings.kt
+++ src/main/kotlin/au/kilemon/messagequeue/settings/MessageQueueSettings.kt
@@ -1,10 +1,7 @@
 package au.kilemon.messagequeue.settings
 
-import au.kilemon.messagequeue.queue.MultiQueue
-import au.kilemon.messagequeue.queue.inmemory.InMemoryMultiQueue
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 
