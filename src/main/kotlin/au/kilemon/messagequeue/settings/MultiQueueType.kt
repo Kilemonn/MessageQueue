181d9eda78a947fc3068463b70407e9fa7d3d465
Kyle Gonzalez <kylegonzalez2255@gmail.com>
2022-09-20 20:37:06 +1000

    Improve SettingsControllerTest. Add docs.

--- src/main/kotlin/au/kilemon/messagequeue/settings/MultiQueueType.kt
+++ src/main/kotlin/au/kilemon/messagequeue/settings/MultiQueueType.kt
@@ -1,5 +1,11 @@
 package au.kilemon.messagequeue.settings
 
+/**
+ * An enum class used to represent the different types of underlying `MultiQueue`.
+ * This will drive which implementation of the `MultiQueue` is initialised and used throughout the application.
+ *
+ * @author github.com/KyleGonzalez
+ */
 enum class MultiQueueType
 {
     IN_MEMORY;
--- src/test/kotlin/au/kilemon/messagequeue/rest/controller/SettingsControllerTest.kt
+++ src/test/kotlin/au/kilemon/messagequeue/rest/controller/SettingsControllerTest.kt
@@ -1,6 +1,9 @@
 package au.kilemon.messagequeue.rest.controller
 
 import au.kilemon.messagequeue.settings.MessageQueueSettings
+import au.kilemon.messagequeue.settings.MultiQueueType
+import com.google.gson.Gson
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,10 +13,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 /**
+ * A test class for the [SettingsController] class. Mainly testing the endpoints in the `RestController`.
+ *
  * @author github.com/KyleGonzalez
  */
 @ExtendWith(SpringExtension::class)
@@ -36,14 +42,19 @@ class SettingsControllerTest
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+    private val gson: Gson = Gson()
+
+    /**
+     * Test [SettingsController.getSettings] and verify the response payload.
+     */
     @Test
     fun testGetSettings()
     {
-        mockMvc.perform(
-            MockMvcRequestBuilders.get(SettingsController.SETTINGS_PATH)
+        val mvcResult: MvcResult =  mockMvc.perform(MockMvcRequestBuilders.get(SettingsController.SETTINGS_PATH)
             .contentType(MediaType.APPLICATION_JSON_VALUE))
             .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.content().json("{\n    \"multiQueueType\": \"IN_MEMORY\"\n}"))
-//            .andExpect(MockMvcResultMatchers.jsonPath("$.muiltiQueueType").value(MultiQueueType.IN_MEMORY.toString()))
+            .andReturn()
+        val settings = gson.fromJson(mvcResult.response.contentAsString, MessageQueueSettings::class.java)
+        Assertions.assertEquals(MultiQueueType.IN_MEMORY, settings.multiQueueType)
     }
 }
