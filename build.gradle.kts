90b0e33530a72ebaa15f34f0929136a29ebad175
Kilemonn <kylegonzalez2255@gmail.com>
2023-09-17 16:52:26 +1000

    Bump to version 0.2.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "au.kilemon"
 // Make sure version matches version defined in MessageQueueApplication
-version = "0.2.0"
+version = "0.2.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
