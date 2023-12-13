f2056e3f620ac5f1ffd18d9226d44d5b8648c199
Kilemonn <kylegonzalez2255@gmail.com>
2023-12-13 22:25:02 +1100

    Add annotations for MessageResponse. Remove extra "subQueue" property in MessageResponse as it is in the message itself. Update tests.

--- src/main/kotlin/au/kilemon/messagequeue/rest/response/AuthResponse.kt
+++ src/main/kotlin/au/kilemon/messagequeue/rest/response/AuthResponse.kt
@@ -4,7 +4,6 @@ import au.kilemon.messagequeue.filter.CorrelationIdFilter
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import io.swagger.v3.oas.annotations.media.Schema
 import org.slf4j.MDC
-import java.util.Optional
 
 /**
  * A response object which wraps the response jwt token.
@@ -15,7 +14,7 @@ import java.util.Optional
 class AuthResponse
 {
     @Schema(title = "The request correlation ID.", example = "1599dcd3-7424-4f97-bc99-b9b3e5c53d59")
-    val correlationId: String? = Optional.ofNullable(MDC.get(CorrelationIdFilter.CORRELATION_ID)).orElse(null)
+    val correlationId: String? = MDC.get(CorrelationIdFilter.CORRELATION_ID)
 
     @Schema(title = "The created access token that can be used to interact with the requested sub-queue.", example = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJraWxlbW9uL21lc3NhZ2UtcXVldWUiLCJTdWItUXVldWUtSWRlbnRpZmllciI6InRlc3RSZXN0cmljdFN1YlF1ZXVlX3Rva2VuR2VuZXJhdGVkIn0.Vr18hd41P0Vp7TOcatWrfBQOQZTEYZx2RAY7A82uwsUeSZfITPpSb3V96YN9IhuAoYWyj2GJRfWvlFLvokNzIQ")
     val token: String
--- src/main/kotlin/au/kilemon/messagequeue/rest/response/MessageResponse.kt
+++ src/main/kotlin/au/kilemon/messagequeue/rest/response/MessageResponse.kt
@@ -3,6 +3,7 @@ package au.kilemon.messagequeue.rest.response
 import au.kilemon.messagequeue.filter.CorrelationIdFilter
 import au.kilemon.messagequeue.message.QueueMessage
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import io.swagger.v3.oas.annotations.media.Schema
 import org.slf4j.MDC
 
 /**
@@ -10,5 +11,19 @@ import org.slf4j.MDC
  *
  * @author github.com/Kilemonn
  */
-@JsonPropertyOrder("correlationId", "subQueue", "message")
-data class MessageResponse(val message: QueueMessage, val subQueue: String = message.subQueue, val correlationId: String? = MDC.get(CorrelationIdFilter.CORRELATION_ID))
+@JsonPropertyOrder("correlationId", "message")
+class MessageResponse
+{
+    @Schema(title = "The request correlation ID.", example = "1599dcd3-7424-4f97-bc99-b9b3e5c53d59")
+    val correlationId: String? = MDC.get(CorrelationIdFilter.CORRELATION_ID)
+
+    val message: QueueMessage
+
+    /**
+     * Not converting to primary constructor, so we can use [Schema] annotations.
+     */
+    constructor(message: QueueMessage)
+    {
+        this.message = message
+    }
+}
