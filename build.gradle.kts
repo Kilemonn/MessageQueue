c5d3a9a7c106a846ff7fda591b0187e240b3cc39
Kilemonn <kylegonzalez2255@gmail.com>
2023-12-13 23:17:51 +1100

    Upgrade dependencies to latest and upgrade Kotlin to 1.9.21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.5"
-    id("io.spring.dependency-management") version "1.0.14.RELEASE"
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.spring") version "1.8.20"
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
     jacoco
 }
 
@@ -18,45 +18,42 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:3.0.6")
-    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.5")
-
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.2.0")
     // JPA dependency
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.0")
+    // No SQL drivers
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-mongodb
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.2.0")
 
     // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-ui
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+    implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.0.6")
+    implementation("com.google.code.gson:gson:2.10.1")
 
-    implementation("com.google.code.gson:gson:2.10")
-
-    compileOnly("org.projectlombok:lombok:1.18.24")
+    compileOnly("org.projectlombok:lombok:1.18.30")
 
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-reflect
-    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.9.21")
 
     // Database drivers
     // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
-    implementation("com.mysql:mysql-connector-j:8.0.31")
+    implementation("com.mysql:mysql-connector-j:8.2.0")
     // https://mvnrepository.com/artifact/org.postgresql/postgresql
-    implementation("org.postgresql:postgresql:42.5.1")
-
-    // No SQL drivers
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-mongodb
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.1.3")
+    implementation("org.postgresql:postgresql:42.7.1")
 
     // JWT token
     // https://mvnrepository.com/artifact/com.auth0/java-jwt
     implementation("com.auth0:java-jwt:4.4.0")
 
     // Test dependencies
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.6")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.0")
     // Required to mock MultiQueue objects since they apparently override a final 'remove(Object)' method.
-    testImplementation("org.mockito:mockito-inline:5.1.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("org.testcontainers:testcontainers:1.19.2")
-    testImplementation("org.testcontainers:junit-jupiter:1.17.5")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.testcontainers:testcontainers:1.19.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.3")
     testImplementation(kotlin("test"))
 }
 
