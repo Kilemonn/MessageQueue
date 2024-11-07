fb10767a5d642ada5eacddad3f7fd992da7181a2
Kilemonn <kylegonzalez2255@gmail.com>
2024-11-07 20:15:01 +0900

    Introduce MessageListResponse so that correlationIds are not duplicated for each message (since it would all be the same id) but only once in the body of the response.

--- src/main/kotlin/au/kilemon/messagequeue/rest/response/MessageListResponse.kt
+++ src/main/kotlin/au/kilemon/messagequeue/rest/response/MessageListResponse.kt
@@ -2,30 +2,27 @@ package au.kilemon.messagequeue.rest.response
 
 import au.kilemon.messagequeue.filter.CorrelationIdFilter
 import au.kilemon.messagequeue.message.QueueMessage
-import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import io.swagger.v3.oas.annotations.media.Schema
 import org.slf4j.MDC
 
 /**
- * A response object which wraps the [QueueMessage].
+ * Response object used when multiple messages are being returned.
  *
  * @author github.com/Kilemonn
  */
-@JsonPropertyOrder("correlationId", "message")
-class MessageResponse
-{
+class MessageListResponse {
     @Schema(title = "The request correlation ID.", example = "1599dcd3-7424-4f97-bc99-b9b3e5c53d59",
         description = "A UUID that uniquely identifies the performed request. This will correlate with any logs written as part of this request for debugging purposes.")
     val correlationId: String? = MDC.get(CorrelationIdFilter.CORRELATION_ID)
 
-    @Schema(description = "The retrieved or created QueueMessage.")
-    val message: QueueMessage
+    @Schema(description = "The retrieved or created QueueMessages.")
+    val messages: List<QueueMessage>
 
     /**
      * Not converting to primary constructor, so we can use [Schema] annotations.
      */
-    constructor(message: QueueMessage)
+    constructor(messages: List<QueueMessage>)
     {
-        this.message = message
+        this.messages = messages
     }
 }
